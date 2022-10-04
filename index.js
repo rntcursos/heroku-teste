a9c4cd2508bde55a5a68ac9e615209fe11370056
rntcursos <rntcursos@gmail.com>
2022-10-04 20:57:11 -0300

    start app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
