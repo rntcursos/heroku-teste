8c3f66ba73ddab621723deef5b8141e4ae0de494
rntcursos <rntcursos@gmail.com>
2022-10-04 21:30:56 -0300

    static css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
